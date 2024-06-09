22626178ab74eb8852a7b56d7aa2f518d5e37fd9
VietAnh2610 <anh.truongviet202@gmail.com>
2024-06-09 23:18:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/counterSlice';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import HomePage from './pages/HomePage/HomePage'
+import OrderPage from './pages/OrderPage/OrderPage'
+import { routes } from './routes';
+import { Header } from 'antd/es/layout/layout';
+import HeaderComponent from './components/HeaderComponent/HeaderComponent';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return (
+              <Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+              } />
+            )
+          })}
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div>NotFoundPage.jsx</div>
+    )
+}
+
+export default NotFoundPage
